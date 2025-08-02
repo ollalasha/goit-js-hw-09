89e5f4c3cb933f07f173626c8aac48505f1c1592
ollalasha <165510780+ollalasha@users.noreply.github.com>
2025-08-02 10:53:06 +0000

    Deploying to gh-pages from @ ollalasha/goit-js-hw-09@da9885f96c60d3f1fa8fd6e1ae2423e56749ce69 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,7 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as a}from"./assets/vendor-CgTBfC_f.js";const i=[{preview:new URL("../img/small1.jpg",import.meta.url).href,original:new URL("../img/large1.jpg",import.meta.url).href,description:"Image 1"},{preview:new URL("../img/small2.jpg",import.meta.url).href,original:new URL("../img/large2.jpg",import.meta.url).href,description:"Image 2"}],t=document.querySelector(".gallery"),m=i.map(({preview:e,original:r,description:l})=>`
+      <li class="gallery-item">
+        <a class="gallery-link" href="${r}">
+          <img class="gallery-image" src="${e}" alt="${l}" />
+        </a>
+      </li>`).join("");t.innerHTML=m;new a(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
